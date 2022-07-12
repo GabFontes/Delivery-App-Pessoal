e01ae727de92972c7a10f9c32f2fa4d5cd41642f
gab <gabrielfontes321@gmail.com>
2022-07-12 19:40:27 -0300

    refactor (back-end/src/database/models):  refactoring products model to common js

--- back-end/src/database/models/products.js
+++ back-end/src/database/models/products.js
@@ -1,24 +1,13 @@
-const { Model, DataTypes } = require('sequelize');
-// const { Sequelize } = require('.');
-const db = require('.');
-
-// const Model = Sequelize.Model
-// const { Sequelize } = require('.');
-
-// const sequelize = require('.');
-
-class Product extends Model { }
-
-Product.init({
-  name: DataTypes.STRING,
-  price: DataTypes.DECIMAL,
-  urlImage: DataTypes.STRING
-}, {
-  sequelize: db,
-  modelName: 'Product',
-  tableName: 'products',
-  underscored: true,
-  timestamps: false,
-});
-
-module.exports = Product;
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    urlImage: DataTypes.STRING
+  }, {
+    modelName: 'Product',
+    tableName: 'products',
+    underscored: true,
+    timestamps: false,
+  });
+  return Product
+};
