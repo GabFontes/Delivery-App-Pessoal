662a1c6ad81e39cc9e3477f1ebdaf470b330cef1
gab <gabrielfontes321@gmail.com>
2022-07-12 19:41:33 -0300

    refactor (back-end/src/database/models):  refactoring users model to common js and fixing associations

--- back-end/src/database/models/users.js
+++ back-end/src/database/models/users.js
@@ -1,18 +1,24 @@
-const { Model, DataTypes } = require('sequelize');
-const db = require('.');
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    tableName: 'users',
+    modelName: 'User',
+    timestamps: false,
+  });
 
-module.exports = class User extends Model { }
-
-User.init({
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  role: DataTypes.STRING
-}, {
-  sequelize: db.sequelize,
-  tableName: 'users',
-  modelName: 'User',
-  timestamps: false,
-});
-
-
+  User.associate = (models) => {
+    User.hasMany(models.Sale, {
+      foreignKey: 'userId',
+      as: 'user'
+    });
+    User.hasMany(models.Sale, {
+      foreignKey: 'sellerId',
+      as: 'seller',
+    });
+  }
+  return User;
+};
