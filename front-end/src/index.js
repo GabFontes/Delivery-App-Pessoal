77446c01765a65aa3bd885905b82b1cc0791db88
renato <renatowillian.2000@gmail.com>
2022-07-12 17:25:57 -0300

    refactor(index.js): including provider and browserRouter to index
    including provider, browserRouter to index and removing webVitals

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Provider from './context/Provider';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
