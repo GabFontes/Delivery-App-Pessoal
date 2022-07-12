7e07d2ace2a05f317044a0b5725afdeb92aacbd8
gab <gabrielfontes321@gmail.com>
2022-07-12 19:42:25 -0300

    feat (back-end/src/routes): implementing UserRouter

--- back-end/src/routes/UserRouter.js
+++ back-end/src/routes/UserRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const UserController = require('../controllers/Users')
-const userMiddleware = require('../middlewares/users.middleware');
+// const userMiddleware = require('../middlewares/users.middleware');
 const router = Router();
 
-router.get('/', userMiddleware, () => console.log('sucesso'));
-router.post('/', () => UserController.create);
+router.get('/', UserController.getAll);
+router.post('/', UserController.create);
 
 module.exports = router;
 
