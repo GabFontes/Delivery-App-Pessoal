e6d17a1f511bb5e706bc728ab449ee0858f86249
gab <gabrielfontes321@gmail.com>
2022-07-12 19:35:32 -0300

    feat((back-end/src/aoi) app.js): adding express.json() in app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,7 +3,7 @@ const router = require('../routes/index');
 const errorMiddleware = require('../middlewares/error.middleware');
 
 const app = express();
-
+app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(router);
