c89573654c02c1abb56b216c0f333c80474291d7
renato <renatowillian.2000@gmail.com>
2022-07-13 00:39:14 -0300

    feat(provider.js): add new states in provider

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 export default function Provider({ children }) {
-  // USER --------------------------------------------------------------------------------
+  // USER ----------------------------------------------
   const [userMail, setUserMail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loginData, setLoginData] = useState({});
+
+  // todas as vezes que a pagina é carregada o setErrorMsg se altera para ''
+  useEffect(() => {
+    setErrorMsg('');
+  }, []);
 
   const contextValue = {
-    // USER ==========
+    // USER -------------------
     userMail,
     setUserMail,
     userPassword,
     setUserPassword,
+    loginData,
+    setLoginData,
+    errorMsg,
+    setErrorMsg,
   };
 
   return (
