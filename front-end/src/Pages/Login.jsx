b3f83e63112d0f56e870f9c4be6f3fc2c534063b
renato <renatowillian.2000@gmail.com>
2022-07-13 14:01:40 -0300

    feat(login.jsx): add redirect to `/login` route
    Redirecting default route (`/`) to `/login` route.Redirecting default route (`/`) to `/login` route

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import LoginForm from '../components/templates/LoginForm';
 
 export default function Login() {
-  // falta redirecionar para login
+  const history = useHistory();
+  const location = useLocation();
+
+  const redirect = () => {
+    const currentPathName = location.pathname.includes('/login');
+    return !currentPathName && history.push('/login');
+  };
+
+  redirect();
 
   return (
     <div>
