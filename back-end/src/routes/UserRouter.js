2917c6e2071493d9704523c021a26fc54c871938
gab <gabrielfontes321@gmail.com>
2022-07-12 23:18:29 -0300

    feat (back-end/src/routes): adding /login route on User Router

--- back-end/src/routes/UserRouter.js
+++ back-end/src/routes/UserRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const UserController = require('../controllers/Users')
-// const userMiddleware = require('../middlewares/users.middleware');
+const UserController = require('../controllers/Users');
+const userMiddleware = require('../middlewares/users.middleware');
+const loginMiddleware = require('../middlewares/login.milddleware');
 const router = Router();
 
 router.get('/', UserController.getAll);
-router.post('/', UserController.create);
+router.post('/', userMiddleware, UserController.create);
+router.post('/login', loginMiddleware, UserController.login);
 
 module.exports = router;
 
