3a9e45e34b2163d7926d445137a9979cb5a056b3
renato <renatowillian.2000@gmail.com>
2022-07-13 00:49:26 -0300

    feat(login.jsx): add LoginForm on Login page

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import LoginForm from '../components/templates/LoginForm';
 
 export default function Login() {
+  // falta redirecionar para login
+
   return (
     <div>
       <h1>Login</h1>
+      <LoginForm />
     </div>
   );
 }
