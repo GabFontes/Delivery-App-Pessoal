4ba7ee5b453ab53df5ca4273395ce5c70bebe727
renato <renatowillian.2000@gmail.com>
2022-07-15 13:53:04 -0300

    feat(app.js): creating to main pages and adding to routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Customer from './Pages/Customer';
+import Administrator from './Pages/Administrator';
+import Seller from './Pages/Seller';
 import NotFound from './Pages/NotFound';
 import './styles/App.css';
 
@@ -10,10 +14,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/login" component={ Login } />
-        {/* <Route exact path="/register" component={ Login } />
-        <Route exact path="/customer/products" component={ Login } />
-        <Route exact path="/seller/orders" component={ Login } />
-        <Route exact path="/admin/manage" component={ Login } /> */}
+        <Route exact path="/register" component={ Register } />
+        <Route exact path="/customer/products" component={ Customer } />
+        <Route exact path="/seller/orders" component={ Seller } />
+        <Route exact path="/admin/manage" component={ Administrator } />
         <Route component={ NotFound } />
       </Switch>
     </div>
---
+++ front-end/src/Pages/Administrator.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Administrator() {
+  return (
+    <div>Admin</div>
+  );
+}
