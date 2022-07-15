9bebcb21b64b9e8d8b0262be0dbbe482a3c6f1be
RuanCunha <ruanvcunha@hotmail.com>
2022-07-15 17:38:19 -0300

    refactor: 💡 adds comments to ItemsCounter atom

--- front-end/src/components/atoms/ItemsCounter.jsx
+++ front-end/src/components/atoms/ItemsCounter.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ItemsCounter() {
   [num, setNum] = useState(0);
 
+  // Constantes com o máximo e o mínimo de itens no estado
+  const maxItems = 10;
+  const minItems = 0;
+
+  // Função que incrementa o número
   incrementNum = () => {
-    if (num <10) setNum(Number(num) + 1);
+    if (num < maxItems) setNum(Number(num) + 1);
   }
 
+  // Função que decrementa o número
   decrementNum = () => {
-    if (num > 0) setNum(num - 1);
+    if (num > minItems) setNum(num - 1);
   }
 
+  // Função que altera o valor do estado com base no input
   handleChange = (e) => {
     setNum(e.target.value);
   }
