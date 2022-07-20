a59b82da1566370dd5ad366ee28e7f22f3513b6e
Thauler <thauler98@gmail.com>
2022-07-20 19:14:19 -0300

    refactor(getallsalesservice.js): adds include option at the query findAll
    adds associations of users, sellers and products with all the datas, at the response.

--- back-end/src/services/Sales/GetAllSalesService.js
+++ back-end/src/services/Sales/GetAllSalesService.js
@@ -1,7 +1,13 @@
-const { Sales } = require('../../database/models');
+const { Sale, Product, User } = require('../../database/models');
 
 const GetAllSalesService = async () => {
-  const results = await Sales.findAll();
+  const results = await Sale.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: User, as: 'seller', attributes: { exclude: ['password'] } },
+      { model: Product, as: 'products', through: { attributes: ['quantity'] } },
+    ],
+  });
   return results;
 };
 
