b37b3d8ca8a368eee11c45fb5232bfcea27c9ebb
Thauler <thauler98@gmail.com>
2022-07-20 19:11:28 -0300

    refactor(getsalebyidservice.js): adds include option on the query
    Use the associations to return all datas of users, products and sellers at response.
    
    Co-authored-by: Gabriel Fontes <gabrielfontes321@gmail.com>
    Co-authored-by: Artur H. de Oliveira <arturholiv@users.noreply.github.com>

--- back-end/src/services/Sales/GetSaleByIdService.js
+++ back-end/src/services/Sales/GetSaleByIdService.js
@@ -1,8 +1,14 @@
-const { Sales } = require('../../database/models');
+const { Sale, Product, User } = require('../../database/models');
 const err = require('../../utils/error.base');
 
 const GetSaleByIdService = async (id) => {
-  const sale = await Sales.findById(id);
+  const sale = await Sale.findByPk(id, {
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: User, as: 'seller', attributes: { exclude: ['password'] } },
+      { model: Product, as: 'products', through: { attributes: ['quantity'] } },
+    ],
+  });
 
   if (!sale) {
     throw err('Could not found a sale with this id', 404);
