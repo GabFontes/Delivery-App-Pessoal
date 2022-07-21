7486bba4d4d331afd516f5ee11895b210c0465af
shinvk <victor.kamiguchi@gmail.com>
2022-07-21 15:22:52 -0300

    feat(frontened/src/context/provider.js): implements a new state for provider

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -2,6 +2,7 @@ import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 import useProductsApi from '../hooks/productHook';
+import useUpdatePriceApi from '../hooks/totalPriceHook';
 
 export default function Provider({ children }) {
   // USER ----------------------------------------------
@@ -11,16 +12,23 @@ export default function Provider({ children }) {
   const [userData, setUserData] = useState({});
 
   // PRODUTOS ---------------------------------------------------------------
+  const [prodCart, setProdCart] = useState(0);
   const [productsData, setProductsData] = useProductsApi(login, userData);
+  const [totalPrice, setTotalPrice] = useUpdatePriceApi(prodCart);
 
   const contexMemo = useMemo(() => ({
+    totalPrice,
+    setTotalPrice,
+    setProdCart,
+    prodCart,
     setlogin,
     login,
     userData,
     setUserData,
     setProductsData,
     productsData,
-  }), [login, productsData, setProductsData, userData]);
+  }), [
+    login, prodCart, productsData, setProductsData, setTotalPrice, totalPrice, userData]);
 
   return (
     <Context.Provider value={ contexMemo }>
