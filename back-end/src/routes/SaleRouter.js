e9c055aa1fff299462672cd85b570a0612b8e828
gab <gabrielfontes321@gmail.com>
2022-07-21 17:42:09 -0300

    feat(back-end/src/routes): calling AuthMiddleware at SaleRouter

--- back-end/src/routes/SaleRouter.js
+++ back-end/src/routes/SaleRouter.js
@@ -4,8 +4,8 @@ const AuthMiddleware = require('../middlewares/authtoken.middleware');
 
 const saleRouter = Router();
 
-saleRouter.get('/', SalesController.getAll);
-saleRouter.get('/:id', SalesController.getById);
+saleRouter.get('/', AuthMiddleware, SalesController.getAll);
+saleRouter.get('/:id', AuthMiddleware, SalesController.getById);
 saleRouter.post('/', AuthMiddleware, SalesController.create);
 saleRouter.delete('/', SalesController.deleted);
 
