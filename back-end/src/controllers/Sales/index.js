8e2715d8917c17699832587da9adcfa5ffc2c23c
gab <gabrielfontes321@gmail.com>
2022-07-21 17:41:11 -0300

    refactor(back-end/src/controllers/Sales): renaming get all sales controller

--- back-end/src/controllers/Sales/index.js
+++ back-end/src/controllers/Sales/index.js
@@ -1,5 +1,5 @@
 const create = require('./CreateSalesController');
-const getAll = require('./GetAllSalesController');
+const getAll = require('./getAllSalesByUserIdController');
 const deleted = require('./DeleteSalesController');
 const getById = require('./GetSaleByIdController');
 
