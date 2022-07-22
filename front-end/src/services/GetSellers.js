183ce6ac710d4420064fb951aec2bd8fad85f2f8
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-07-22 19:21:22 -0300

    feat: 🎸 get sellers[

---
+++ front-end/src/services/GetSellers.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+async function GetSellers(token) {
+  try {
+    const url = 'http://localhost:3001/seller';
+    const resp = await axios.get(url, { headers: { authorization: token } });
+    const { data } = resp;
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export default GetSellers;
+
+//   [
+//   {
+//     id: 2,
+//     name: 'Fulana Pereira',
+//     email: 'fulana@deliveryapp.com',
+//     password: '3c28d2b0881bf46457a853e0b07531c6',
+//     role: 'seller',
+//   },
+//   {
+//     id: 5,
+//     name: 'Gabriel Fontes',
+//     email: 'gabfontes@email.com',
+//     password: 'caa9c8f8620cbb30679026bb6427e11f',
+//     role: 'seller',
+//   },
+// ];
