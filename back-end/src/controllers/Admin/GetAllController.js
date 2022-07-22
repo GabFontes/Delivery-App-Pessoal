c1c0eb7ac81cffe265bea237cc4a4705deb89a1c
gab <gabrielfontes321@gmail.com>
2022-07-21 22:20:04 -0300

    feat(back-end/src/controllers/Admin): implements get all users controller for admin route

---
+++ back-end/src/controllers/Admin/GetAllController.js
@@ -0,0 +1,13 @@
+const AdminService = require('../../services/Admin');
+
+const GetAllController = async (req, res, next) => {
+  try {
+    const users = await AdminService.getAll();
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports = GetAllController;
