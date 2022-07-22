55ea8719c3e9ac87d8b6c3b815182461e4eb6aef
Thauler <thauler98@gmail.com>
2022-07-22 18:29:08 -0300

    refactor(back-end/src/services/Admin): implements at service layer findall on users with a filter of the role that have to be not  equal administrator.
    Co-authored-by: Gabriel Fontes <gabrielfontes321@gmail.com>

--- back-end/src/services/Admin/GetAllService.js
+++ back-end/src/services/Admin/GetAllService.js
@@ -1,7 +1,8 @@
+const { Op } = require('sequelize');
 const { User } = require('../../database/models');
 
 const GetAllUserService = async () => {
-  const results = await User.findAll();
+  const results = await User.findAll({ where: { id: { [Op.ne]: 'administrator' } } });
   return results;
 };
 
