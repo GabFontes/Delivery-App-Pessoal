b64c8143fe024ca0e5048f6989cdf47d51a9dbb1
Renato Adorno <renattoadorno@gmail.com>
2022-07-22 17:30:26 -0300

    feat(src/components/templates/orderdetails.js): adds template OrderDetail.js

---
+++ front-end/src/components/templates/OrderDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardDetails from '../molecules/CardDetails';
+
+export default function OrderDetails() {
+  // recebe id
+  return (
+    <div>
+      <CardDetails />
+    </div>
+  );
+}
