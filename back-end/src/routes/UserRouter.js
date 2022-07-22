c26f49c9ada101f0276d4cf7a721df7e1e2e18f2
Thauler <thauler98@gmail.com>
2022-07-22 18:13:52 -0300

    refactor(back-end/src/routes): adds auth middleware of JWT verify.
    Co-authored-by: Gabriel Fontes <gabrielfontes321@gmail.com>

--- back-end/src/routes/UserRouter.js
+++ back-end/src/routes/UserRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const UserController = require('../controllers/Users');
+const UserMiddleware = require('../middlewares/users.middleware');
 
 const userRouter = Router();
 
-userRouter.post('/', UserController.create);
+userRouter.post('/', UserMiddleware, UserController.create);
 
 module.exports = userRouter;
