b14235044c066cfadf1d06978dec304d3db3d91d
Renato Adorno <renattoadorno@gmail.com>
2022-07-22 17:28:54 -0300

    feat(src/components/molecules/carddetails.js): adds CardDetail.js

---
+++ front-end/src/components/molecules/CardDetails.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../atoms/Button';
+
+export default function CardDetails({
+  // Date,
+  // nOrder,
+  i,
+  productName,
+  quantity,
+  price,
+  subtotal,
+  // click,
+  // id,
+}) {
+  // ALTERAR DATA TESTID
+  return (
+    <div>
+
+      <div style={ { display: 'flex' } }>
+        <h2 data-testid="customer_order_details__element-order-table-item-number">
+          Numero do Pedido
+        </h2>
+        <h2 data-testid="customer_order_details__element-order-table-item-number">
+          Nome Vendedora
+        </h2>
+        <h2 data-testid="customer_order_details__element-order-table-item-number">
+          Data
+        </h2>
+        <h2 data-testid="customer_order_details__element-order-table-item-number">
+          Status
+        </h2>
+        <h2>
+          Status
+        </h2>
+        <Button
+          nameView="MARCAR COMO ENTREGUE"
+          disabled={ false }
+          testid="customer_order_details__button-delivery-check"
+          // onClick={ () => click(id) }
+          name="btnEntregue"
+        />
+      </div>
+
+      <div style={ { display: 'flex' } }>
+        <p>
+          Item
+        </p>
+        <p>
+          Descrição
+        </p>
+        <p>
+          Quantidade
+        </p>
+        <p>
+          Valor Unitário
+        </p>
+        <p>
+          Sub-total
+        </p>
+      </div>
+
+      <div style={ { display: 'flex' } }>
+        <h4
+          data-testid={ `customer_order_details__element-order-table-item-number-${i}` }
+        >
+          { i }
+        </h4>
+        <h4 data-testid={ `customer_order_details__element-order-table-name-${i}` }>
+          { productName }
+        </h4>
+        <h4 data-testid={ `customer_order_details__element-order-table-quantity-${i}` }>
+          { quantity }
+        </h4>
+        <h4 data-testid={ `customer_order_details__element-order-total-price-${i}` }>
+          { price }
+        </h4>
+        <h4 data-testid={ `customer_order_details__element-order-table-sub-total-${i}` }>
+          { subtotal }
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+CardDetails.propTypes = {
+  price: PropTypes.string.isRequired,
+  subtotal: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired,
+  productName: PropTypes.string.isRequired,
+  i: PropTypes.number.isRequired,
+  // click: PropTypes.func.isRequired,
+  // id: PropTypes.number.isRequired,
+};
