1190740ad78bd0e8a0f8ff9cdde6e456af1ab702
shinvk <victor.kamiguchi@gmail.com>
2022-07-25 16:20:47 -0300

    refactor(01 e 02 tests): add constants for lint

--- front-end/src/tests/01.reqGet.test.js
+++ front-end/src/tests/01.reqGet.test.js
@@ -5,30 +5,31 @@ import { getMockAxios } from './mocks/mocksFetchs';
 import productsResponse from './mocks/Products/ProductsMock';
 import sellerGetAllResponse from './mocks/Seller/SellerMock';
 
+const invalidToken = 'invalid token';
 
-jest.mock("axios");
-describe('Axios - Testa as requisições GET', () => {
+jest.mock('axios');
+describe('01 - Axios - Testa as requisições GET', () => {
   describe('Get/products -> axios GetProduct(token)', () => {
     it('01. Sucesso -> A função retorna o mock esperado com a url esperada', async () => {
       axios.get.mockImplementation(getMockAxios);
       const result = await GetProduct('sucessfulltoken');
       expect(axios.get).toHaveBeenCalled();
       expect(axios.get).toHaveBeenCalledWith('http://localhost:3001/products', {
-        "headers": {"authorization": "sucessfulltoken"}
+        headers: { authorization: 'sucessfulltoken' },
       });
-      expect(result).toEqual(productsResponse)
+      expect(result).toEqual(productsResponse);
     });
 
     it('02. Falha -> retorna o erro', async () => {
-      axios.get.mockRejectedValueOnce(new Error('invalid token'))
+      axios.get.mockRejectedValueOnce(new Error(invalidToken));
       try {
         await GetProduct('failedtoken');
-      } catch(error) {
+      } catch (error) {
         expect(axios.get).toHaveBeenCalled();
         expect(axios.get).toHaveBeenCalledWith('http://localhost:3001/products', {
-          "headers": {"authorization": "failedtoken"}
+          headers: { authorization: 'failedtoken' },
         });
-        expect(error.message).toBe('invalid token')
+        expect(error.message).toBe(invalidToken);
       }
     });
   });
@@ -39,21 +40,21 @@ describe('Axios - Testa as requisições GET', () => {
       const result = await GetSellers('sucessfulltoken');
       expect(axios.get).toHaveBeenCalled();
       expect(axios.get).toHaveBeenCalledWith('http://localhost:3001/seller', {
-        "headers": {"authorization": "sucessfulltoken"}
+        headers: { authorization: 'sucessfulltoken' },
       });
-      expect(result).toEqual(sellerGetAllResponse)
+      expect(result).toEqual(sellerGetAllResponse);
     });
 
     it('04. Falha -> retorna o erro', async () => {
-      axios.get.mockRejectedValueOnce(new Error('invalid token'))
+      axios.get.mockRejectedValueOnce(new Error(invalidToken));
       try {
         await GetSellers('failedtoken');
-      } catch(error) {
+      } catch (error) {
         expect(axios.get).toHaveBeenCalled();
         expect(axios.get).toHaveBeenCalledWith('http://localhost:3001/seller', {
-          "headers": {"authorization": "failedtoken"}
+          headers: { authorization: 'failedtoken' },
         });
-        expect(error.message).toBe('invalid token')
+        expect(error.message).toBe('invalid token');
       }
     });
   });
--- front-end/src/tests/02.reqPost.test.js
+++ front-end/src/tests/02.reqPost.test.js
@@ -3,27 +3,26 @@ import { postMockAxios } from './mocks/mocksFetchs';
 import PostLogin from '../services/PostLogin';
 import PostRegister from '../services/PostRegister';
 import PostSale from '../services/PostSale';
-import { longinResponse, loginRequest} from './mocks/Login/LoginMock';
+import { longinResponse, loginRequest } from './mocks/Login/LoginMock';
 import registerResponse, { registerRequest } from './mocks/Register/UserMock';
 import { CreateSaleRequest, CreateSaleResponse } from './mocks/Sales/SalesMock';
 
-
-jest.mock("axios");
-describe('Axios - Testa as requisições POST', () => {
+jest.mock('axios');
+describe('02 - Axios - Testa as requisições POST', () => {
   describe('post/login -> axios PostLogin(user)', () => {
     it('05. Sucesso -> A função retorna o mock esperado com a url esperada', async () => {
       axios.post.mockImplementation(postMockAxios);
       const result = await PostLogin(loginRequest);
       expect(axios.post).toHaveBeenCalled();
-      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/login', {"email": "thauler98@email.com", "password": "testando"});
-      expect(result).toEqual(longinResponse)
+      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/login', { email: 'thauler98@email.com', password: 'testando' });
+      expect(result).toEqual(longinResponse);
     });
 
     it('06. Falha -> retorna o erro', async () => {
-        axios.post.mockRejectedValue(postMockAxios('aa'))
-        const result = PostLogin('e');
-        expect(axios.post).toHaveBeenCalled();
-        await expect(result).rejects.toThrow()
+      axios.post.mockRejectedValue(postMockAxios('aa'));
+      const result = PostLogin('e');
+      expect(axios.post).toHaveBeenCalled();
+      await expect(result).rejects.toThrow();
     });
   });
 
@@ -32,15 +31,15 @@ describe('Axios - Testa as requisições POST', () => {
       axios.post.mockImplementation(postMockAxios);
       const result = await PostRegister(registerRequest);
       expect(axios.post).toHaveBeenCalled();
-      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/register', {"email": "thauler98@email.com", "name": "Thauler Mayrink", "password": "testando"});
+      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/register', { email: 'thauler98@email.com', name: 'Thauler Mayrink', password: 'testando' });
       expect(result).toEqual(registerResponse);
     });
 
     it('08. Falha -> retorna o erro', async () => {
-      axios.post.mockRejectedValue(postMockAxios('aa'))
+      axios.post.mockRejectedValue(postMockAxios('aa'));
       const result = PostRegister('e');
       expect(axios.post).toHaveBeenCalled();
-      await expect(result).rejects.toThrow()
+      await expect(result).rejects.toThrow();
     });
   });
 
@@ -49,15 +48,15 @@ describe('Axios - Testa as requisições POST', () => {
       axios.post.mockImplementation(postMockAxios);
       const result = await PostSale(CreateSaleRequest, 'token');
       expect(axios.post).toHaveBeenCalled();
-      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/sales', CreateSaleRequest, {"headers": {"authorization": "token"}})
+      expect(axios.post).toHaveBeenCalledWith('http://localhost:3001/sales', CreateSaleRequest, { headers: { authorization: 'token' } });
       expect(result).toEqual(CreateSaleResponse);
     });
 
     it('10. Falha -> retorna o erro', async () => {
-      axios.post.mockRejectedValue(postMockAxios('aa'))
+      axios.post.mockRejectedValue(postMockAxios('aa'));
       const result = PostSale('e');
       expect(axios.post).toHaveBeenCalled();
-      await expect(result).rejects.toThrow()
+      await expect(result).rejects.toThrow();
     });
   });
 });
