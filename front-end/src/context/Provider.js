f6526fef40d79c921f00ef0da1a0b0b056c59232
shinvk <victor.kamiguchi@gmail.com>
2022-07-25 22:25:28 -0300

    feat(provider.js): implements login for use setLogin from localStor, and use hook for set userdata

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 import useProductsApi from '../hooks/productHook';
@@ -6,17 +6,18 @@ import useUpdatePriceApi from '../hooks/totalPriceHook';
 import useSellerApi from '../hooks/sellerHook';
 import useOrdersApi from '../hooks/ordersHook';
 import useOrdersByIdApi from '../hooks/ordersByIdHook';
+import useSetUser from '../hooks/useUserHook';
+import { getIsLogger } from '../services/setUserLocal';
 
 export default function Provider({ children }) {
   // USER --------------------------------------------------------------------
   const [login, setlogin] = useState(false);
-
   // estados da requisição --------
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useSetUser(login);
 
   // PRODUTOS ----------------------------------------------------------------
   const [prodCart, setProdCart] = useState(0);
-  const [productsData, setProductsData] = useProductsApi(login, userData);
+  const [productsData, setProductsData] = useProductsApi(login);
   const [totalPrice, setTotalPrice] = useUpdatePriceApi(prodCart);
 
   // Seller ------------------------------------------------------------------
@@ -30,6 +31,11 @@ export default function Provider({ children }) {
   const [orderById, setOrderById] = React.useState(false);
   const [pId, setPId] = React.useState(0);
   const [ordersByIdData, setOrdersByIdData] = useOrdersByIdApi(pId, orderById, userData);
+
+  useEffect(() => {
+    const isLogged = getIsLogger();
+    setlogin(isLogged);
+  }, []);
 
   const contexMemo = useMemo(() => ({
     pId,
@@ -57,18 +63,22 @@ export default function Provider({ children }) {
     ordersData,
     setordersData,
   }), [
-    ordersByIdData,
-    setOrdersByIdData,
     pId,
     orderById,
-    cart,
-    login,
+    ordersByIdData,
+    setOrdersByIdData, sellerData, setSellerData, totalPrice, setTotalPrice,
     prodCart,
+    login,
+    cart,
+    userData,
+    setUserData,
+    setProductsData,
+    productsData,
+    orders,
     ordersData,
     setordersData,
-    orders,
-    productsData,
-    sellerData, setProductsData, setSellerData, setTotalPrice, totalPrice, userData]);
+
+  ]);
 
   return (
     <Context.Provider value={ contexMemo }>
